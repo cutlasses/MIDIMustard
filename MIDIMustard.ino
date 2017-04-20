67f4d919f8d09785b77984e5a6f5d7c8ff8d8937
Cutlasses <spitkethly@gmail.com>
2017-04-20 21:45:39 +0100

    bug fixes

--- MIDIMustard.ino
+++ MIDIMustard.ino
@@ -9,52 +9,65 @@ const int MIDI_CHANNEL = 2;
 
 class MIDI_DIAL
 {
+  static const int NUM_SAMPLES = 20;
+  
   const int     m_data_pin;
   const int     m_midi_cc;
+  int           m_values[NUM_SAMPLES];
+  int           m_value_index;
   int           m_current_value;
     
 public:
 
-   MIDI_DIAL( int data_pin, int midi_cc ) :
+  MIDI_DIAL( int data_pin, int midi_cc ) :
     m_data_pin( data_pin ),
     m_midi_cc( midi_cc ),
-    m_current_value( 0 )
+    m_values(),
+    m_value_index(0),
+    m_current_value(0)
   {
-  
+    for( int i = 0; i < NUM_SAMPLES; ++i )
+    {
+      m_values[i] = 0;
+    }
   }
 
-  bool update()
+  void update()
   {
-    const int NUM_SAMPLES( 3 );
-    int new_value( 0 );
-    for( int i = 0; i < NUM_SAMPLES; ++ i )
+    int average_value = 0;
+    for( int i = 0; i < NUM_SAMPLES; ++i )
     {
-      new_value += analogRead( m_data_pin );  
+      average_value += m_values[i];
     }
-    new_value /= NUM_SAMPLES;
-  
-    if( new_value != m_current_value )
-    {
-      static const float scale        = 127.0f / 1024.0f;
-      const int current_value_scaled  = m_current_value * scale;
-      const int new_value_scaled      = new_value * scale;
-      if( current_value_scaled != new_value_scaled )
-      {
-        usbMIDI.sendControlChange( m_midi_cc, new_value_scaled, MIDI_CHANNEL );
-      }
+    average_value /= NUM_SAMPLES;
 
-      m_current_value = new_value;
-      
-      return true;
+    const int THRESHOLD(8);
+    const int new_value = analogRead( m_data_pin );
+    m_values[m_value_index] = new_value;
+    ++m_value_index;
+    if( m_value_index >= NUM_SAMPLES )
+    {
+      m_value_index = 0;
     }
+
+    const int delta = abs( new_value - average_value );
+    if( delta > THRESHOLD )
+    { 
+      const int average_scaled  = average_value / 8;
+      const int current_scaled  = m_current_value / 8;
+      if( average_scaled != current_scaled )
+      {
+        usbMIDI.sendControlChange( m_midi_cc, average_scaled, MIDI_CHANNEL );
   
-    return false;
+        m_current_value = average_value;
+      }
+    }
   }
 };
 
 ///////////////////////////////////////////////
 
-MIDI_DIAL g_dials[NUM_DIALS] = { MIDI_DIAL( 0, 14 ), MIDI_DIAL( 1, 15 ) };  // use MIDI CC 14 and 15, marked as undefined, ensures it doesn't clash with my midi pedal
+MIDI_DIAL g_dials[NUM_DIALS] = { MIDI_DIAL( 0, 41 ), MIDI_DIAL( 1, 42 ) };  // use MIDI CC 14 and 15, marked as undefined, ensures it doesn't clash with my midi pedal
 
 ///////////////////////////////////////////////
 
@@ -71,5 +84,8 @@ void loop()
     g_dials[i].update();
   }
 
-  delay(5); // 5ms
+  // consume MIDI messages
+  while( usbMIDI.read() );
+  
+  delay(10); // 10ms
 }
